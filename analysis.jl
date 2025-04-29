e1a82d1670fb9177f29683b951e14651f1bc5f39
Martin Hinsch <hinsch.martin@gmail.com>
2025-04-29 10:51:45 +0100

    sample coop by distance

--- analysis.jl
+++ analysis.jl
@@ -49,11 +49,28 @@ const MMA = MaxMinAcc{Float64}
         push!(cond, p.local_cond)
     end
 
+    range = pars.spread_exchange * pars.effect_radius
+    coop_d = Float64[]
+    n_d = Int[]
+    for x in 0:2*range:pars.sz_x/2
+        push!(coop_d, 0.0)
+        push!(n_d, 0)
+        for p in iter_circle(world.pop_cache, (pars.sz_y/2, pars.sz_x/2+x), range)
+            coop_d[end] += p.coop
+            n_d[end] += 1
+        end
+        if n_d[end] > 0
+            coop_d[end] /= n_d[end]
+        end
+    end
+        
     @record "cor_dens_exch" Float64 (isempty(dens) ? 0.0 : cor(dens, exch))
     @record "cor_dens_prov" Float64 (isempty(dens) ? 0.0 : cor(dens, prov))
     @record "cor_cond_prov" Float64 (isempty(cond) ? 0.0 : cor(cond, prov))
     @record "cor_dist_coop" Float64 (isempty(dist) ? 0.0 : cor(dist, coop))
     @record "cor_dens_coop" Float64 (isempty(dens) ? 0.0 : cor(dens, coop))
+    @record "coop_dist_sample" Vector{Float64} coop_d
+    @record "n_dist_sample" Vector{Float64} n_d
 end
 
 
