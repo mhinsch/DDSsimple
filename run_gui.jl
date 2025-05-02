55be4696df6c2379b6a71a9eede9360f4c12d3bf
Martin Hinsch <hinsch.martin@gmail.com>
2025-05-02 15:41:24 +0100

    misc gui

--- run_gui.jl
+++ run_gui.jl
@@ -63,8 +63,9 @@ function run(model, gui, graphs1, graphs2, graphs3, logfile, max_step = 0.1)
 					set_data!(graphs2[3], data.density_all, data.exchange_all)
 					set_data!(graphs2[4], data.density_all, data.storage_all)
 
-					set_data!(graphs3[1], data.distance_all, data.condition_all)
-					set_data!(graphs3[2], data.distance_all, data.coop_all)
+					add_value!(graphs3[1], data.coop.mean)
+					#set_data!(graphs3[1], data.distance_all, data.condition_all)
+					#set_data!(graphs3[2], data.distance_all, data.coop_all)
 
 					#set_data!(graphs3[1], [0.0, 0.0]) 
 					#set_data!(graphs3[2], data.condition_all, data.provision_all)
@@ -155,8 +156,9 @@ const graphs2 = [
 
 const graphs3 = [
 #	Graph{Float64}(WHITE, ""), 
-	Graph{Float64}(rgb(255, 0, 200), "dist->condition", method=:scatter),
-	Graph{Float64}(rgb(255, 128, 0), "dist->coop", method=:scatter)]
+	#Graph{Float64}(rgb(255, 0, 200), "dist->condition", method=:scatter),
+	#Graph{Float64}(rgb(255, 128, 0), "dist->coop", method=:scatter)]
+	Graph{Float64}(rgb(255, 128, 0), "mean coop")]
 #	Graph{Float64}(rgb(0, 240, 255), "cond -> exch", method=:scatter)]
 #const graphs3 = [Graph{Float64}(WHITE, "density", method = :scatter)]
 
